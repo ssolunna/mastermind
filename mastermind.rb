7606bcfa145ee79486174e92540399ec0fd7dbae
ssolunna <ssolunna29@gmail.com>
2023-02-14 13:42:49 -0500

    Check for game winner

--- mastermind.rb
+++ mastermind.rb
@@ -2,9 +2,34 @@
 
 # Game: Mastermind
 
+module CodePeg
+  CODEPEGS = %w[yellow green red blue purple pink].freeze
+
+  def self.info
+    puts "Code pegs: #{CODEPEGS.join(' ')}"
+    puts 'Syntax: space-separated words (e.g. color color color color)'
+    puts
+  end
+
+  def get_codepegs(quantity, random = nil)
+    if random
+      CODEPEGS.sample(quantity)
+    else
+      gets.chomp.split(' ')
+    end
+  end
+
+  def self.valid_color?(codepegs)
+    codepegs.all? { |codepeg| CODEPEGS.include?(codepeg) }
+  end
+end
+
 class Game
+  include CodePeg
+
   attr_accessor :codemaker, :codebreaker
-  attr_reader :rows
+
+  attr_reader :rows, :spaces
 
   def initialize(rows, spaces)
     @rows = rows
@@ -27,40 +52,36 @@ class Game
     @codebreaker.guess = @codebreaker.get_codepegs(@spaces)
   end
 
-  def valid?(codepegs)
-    CodePeg.valid_color?(codepegs) && codepegs.length == @spaces
-  end
-end
-
-module CodePeg
-  CODEPEGS = %w[yellow green red blue purple pink].freeze
-
-  def self.info
-    puts "Code pegs: #{CODEPEGS.join(' ')}"
-    puts 'Syntax: space-separated words (e.g. color color color color)'
+  def valid_guess?
+    CodePeg.valid_color?(@codebreaker.guess) &&
+      @codebreaker.guess.length == @spaces
   end
 
-  def get_codepegs(quantity, random = nil)
-    if random
-      CODEPEGS.sample(quantity)
-    else
-      gets.chomp.split(' ')
-    end
+  def invalid_guess
+    puts
+    puts "Couldn't get your guess."
+    puts "You must type #{@spaces} colors from the code pegs (space-separated)."
   end
 
-  def self.valid_color?(codepegs)
-    codepegs.all? { |codepeg| CODEPEGS.include?(codepeg) }
+  def codebreaker_won?
+    @codebreaker.winner = true if @codebreaker.guess == @codemaker.pattern
+  end
+
+  def winner
+    puts
+    puts @codebreaker.winner ? 'You won! You got it right.' :
+      "Game over. The pattern was: #{@codemaker.pattern.join(' ')}"
   end
 end
 
 class CodeMaker
   include CodePeg
 
-  attr_accessor :pattern
+  attr_accessor :pattern, :winner
 
   def initialize(name)
     @name = name
-    @pattern = nil
+    @pattern = []
     @winner = false
   end
 end
@@ -68,26 +89,37 @@ end
 class CodeBreaker
   include CodePeg
 
-  attr_accessor :guess
+  attr_accessor :guess, :winner
 
   def initialize(name)
     @name = name
-    @guess = nil
+    @guess = []
     @winner = false
   end
 end
 
 # Display documentation
 CodePeg.info
-puts
 
 # Start game with number of (rows, spaces)
 game = Game.new(12, 4)
 game.get_players
 game.make_pattern
 
+# Loop through Game rows making guesses
 game.rows.times do |number|
   row = number + 1
+  last_row = game.rows
   print "[Row #{row}] "
   game.make_guess
+
+  if game.valid_guess?
+    if game.codebreaker_won? || row == last_row
+      game.winner # Display game winner
+      break
+    end
+  else
+    game.invalid_guess
+    break
+  end
 end
